55954f0bb0b8cce565d89ee9d804fb883ceb9618
Web <web@it.step>
2019-03-17 19:48:01 +0300

    liasrchy

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+   
+   
+   
+   
+   
+   <?php 
+    
+    $var1 = "Hello";
+    $var2 = "world!";
+    
+    echo $var1." ".$var2;
+    
+     die();
+    
+     var_dump($var1);
+    
+   
+    
+    
+    ?> <br>
+   
+  
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+
+   
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
